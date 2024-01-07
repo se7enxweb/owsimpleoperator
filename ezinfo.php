df17f6cd9276572c851ad3e51c65ea973ed78d13
7x <info@se7enx.com>
2024-01-07 12:22:35 -0800

    Update ezinfo.php increment version number

--- ezinfo.php
+++ ezinfo.php
@@ -13,7 +13,7 @@ class owSimpleOperatorInfo
     public static function info()
     {
         return array( 'Name' => "<a href='https://github.com/se7enxweb/owsimpleoperator'>OW Simple Operator</a>",
-                      'Version' => "1.0.1",
+                      'Version' => "1.2.0",
                       'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>7x</a> and Brookins Consulting, 2011 - 2014 <a href='https://www.openwide.fr' title='OPEN WIDE'>OPEN WIDE</a>",
                       'License' => "GNU General Public License (or Later Version)",
                       'info_url' => "https://packagist.org/packages/se7enxweb/owsimpleoperator"
